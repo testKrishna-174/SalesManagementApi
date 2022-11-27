ec2162aefc50e581a83cbc9f5d04a653bd1d6890
Krishna Kumar Madathil <KrishnaKumar.Madathil@sirva.com>
2022-11-27 18:15:11 +0530

    File Upload Changes

---
+++ SalesManagement/Controllers/FileUploadController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesManagement_API.Models;
+using SalesManagement_API.BAL;
+
+namespace SalesManagement_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public FileUploadController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost]
+        [Route("InsertFileUpload")]
+        public JsonResult InsertDesignation(FileUpload fileUploadDetais)
+        {
+            FileUpload_BAO bao = new FileUpload_BAO(_configuration);
+            string result = bao.InsertFileUploadDetails(fileUploadDetais);
+            return new JsonResult(result);
+
+        }
+    }
+}
---
+++ SalesManagement/Models/FileUpload.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesManagement_API.Models
+{
+    public class FileUpload
+    {
+		public long FileId { get; set; }
+		public string FileName { get; set; }
+		public string LocalFilePath { get; set; }
+		public DateTime UploadedOn { get; set; }
+		public long UploadedBy { get; set; }
+		public bool IsProcessed { get; set; }
+		public string ErrorMessage { get; set; }
+		public List<FileUploadStaging> stagingFileDetails { get; set; }
+	}
+}
